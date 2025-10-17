0f440cba07d3acae4421a6dd9db8489f8033024b
mounaimMoussaoui <moussaouiabdennaim@gmail.com>
2025-10-17 23:15:25 +0100

    create the form login with the best practice of Formik Library and resolve the problem of page Not Founded

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,61 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { auth  } from "../firebase.js";
+import {Form, Formik, useField} from "formik";
 import { signInWithEmailAndPassword } from "firebase/auth"
+import {signUpSchema} from "../schemas/SignUpSchema.js";
+import { TbLockPassword } from "react-icons/tb";
+import { MdOutlineMail } from "react-icons/md";
+
+
+const MyTextField = ({label, children, ...props}) => {
+
+    const [field, meta] = useField(props);
+    return  (<>
+        <label htmlFor={props.id || props.name}
+            className={"text-xl font-bold"}
+            >{label}</label>
+        <div className="relative w-full h-[50px]">
+            <span className={`absolute top-[50%] left-[8px] translate-y-[-50%] text-2xl z-10 ${meta.touched && meta.error ? "text-red-500" : "text-green-500"}`}>{children}</span>
+            <input
+                className={`input-form py-3 px-[40px] shadow-lg rounded-sm absolute min-w-full min-h-full transition ease-in-out focus:outline-none focus:border-b-2 ${meta.touched && meta.error ? 'focus:border-b-red-500' : 'border-green-500'}`}
+                {...field} {...props} />
+        </div>
+        {
+            meta.touched && meta.error ? ( <span className={"text-sm text-red-500 "}>{meta.error}</span> ) : null
+        }
+    </>)
+};
 
 
 export const Login = React.memo(() => {
-    useEffect(() => {
-        console.log(auth)
-        signInWithEmailAndPassword(auth, "testing@example.com", "testing123").then( (res) => {
-            console.log(res)
-        } ).catch((rej) => {
-            console.error(rej);
-        })
-    }, []);
-
-    return <h1>Login</h1>
+    return <>
+        <h1 className={"text-5xl font-bold w-fit mx-auto mt-5"}>Login</h1>
+        <Formik
+        initialValues={{
+            email: '',
+            password: ''
+        }}
+        validationSchema={signUpSchema}
+        onSubmit={(values, { setSubmitting }) => {
+            signInWithEmailAndPassword(auth, values.email, values.password).then( (res) => {
+                console.log(res)
+            } ).catch((rej) => {
+                console.error(rej);
+            });
+            setSubmitting(false);
+        }}
+        >
+            <Form className={"flex flex-col gap-4 max-w-[500px] mx-auto mt-[50px]"}>
+                <MyTextField type={"email"} id="emailField" label="Email" name={"email"} placeholder={"email@example.com"} >
+                    <MdOutlineMail />
+                </MyTextField>
+                <MyTextField type={"password"} id="passwordField" label="Password" name={"password"} placeholder={"********"}>
+                    <TbLockPassword />
+                </MyTextField>
+                <button type={"submit"}
+                className={"py-3 px-5 rounded shadow-lg bg-black font-bold text-white"}
+                >Login</button>
+            </Form>
+        </Formik>
+    </>
 });
