5497ef9eee015e02c17eb1d1e11c3ae24dcaed42
mounaimMoussaoui <moussaouiabdennaim@gmail.com>
2025-10-18 20:00:06 +0100

    styling the not found page and added some icons to sign up form

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { TbError404Off } from "react-icons/tb";
+import {useNavigate} from "react-router-dom";
 
 
 export const NotFound = React.memo(() => {
-    return <h1>Not Found</h1>
+    const navigate = useNavigate();
+
+    return <div className="not-found flex flex-col items-center gap-5 mt-[100px] mx-auto w-fit">
+        <TbError404Off className="icon text-7xl text-red-500" />
+        <h1 className={"text-5xl text-gray-200 font-bold"}>Page Not Found</h1>
+        <button type={"button"} className={"px-5 py-3 shadow-red-500 rounded shadow-sm font-bold text-red-600"} onClick={() => { navigate("/"); }}>Contact US</button>
+    </div>
 });
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,6 +3,8 @@ import { auth } from "../firebase.js";
 import { useFormik } from "formik";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import {signUpSchema} from "../schemas/SignUpSchema.js";
+import {MdOutlineMail} from "react-icons/md";
+import {TbLockPassword} from "react-icons/tb";
 
 export const Signup = React.memo(() => {
 
@@ -30,25 +32,31 @@ export const Signup = React.memo(() => {
         <form onSubmit={formik.handleSubmit} className="mt-[100px] max-w-[500px] mx-auto form flex flex-col gap-4 items-center">
             <div className="group flex flex-col gap-2 w-full">
                 <label htmlFor="emailField" className={"font-bold"}>Email :</label>
-                <input type="email" id={"emailField"} name="email"
-                       value={formik.values.email}
-                       onChange={formik.handleChange}
-                       onBlur={formik.handleBlur}
-                       placeholder={"email@example.com"}
-                       className={"shadow-sm shadow-black py-3 px-5 rounded focus:outline-none transition ease-in-out focus:shadow-cyan-800"}/>
+                <div className={"relative w-full min-h-12"}>
+                    <input type="email" id={"emailField"} name="email"
+                           value={formik.values.email}
+                           onChange={formik.handleChange}
+                           onBlur={formik.handleBlur}
+                           placeholder={"email@example.com"}
+                           className={"shadow-sm shadow-black py-3 px-11 w-full h-full absolute rounded focus:outline-none transition ease-in-out focus:shadow-cyan-800"}/>
+                    <MdOutlineMail className={"absolute text-2xl left-[10px] top-[50%] -translate-y-[50%]"} />
+                </div>
                 { formik.errors.email && formik.touched.email ? <span className={"text-red-500 text-sm"}>{formik.errors.email}</span> : null}
             </div>
             <div className="group flex flex-col gap-2 w-full">
                 <label htmlFor="passField" className={"font-bold"}>Password :</label>
-                <input type="password" id={"passField"} name="password"
-                       value={formik.values.password}
-                       onBlur={formik.handleBlur}
-                       onChange={formik.handleChange}
-                       placeholder={"password"}
-                       className={"shadow-sm shadow-black py-3 px-5 rounded focus:outline-none transition ease-in-out focus:shadow-cyan-800"}/>
+                <div className={"relative w-full min-h-12"}>
+                    <input type="password" id={"passField"} name="password"
+                           value={formik.values.password}
+                           onBlur={formik.handleBlur}
+                           onChange={formik.handleChange}
+                           placeholder={"password"}
+                           className={"shadow-sm shadow-black py-3 px-11 w-full h-full absolute rounded focus:outline-none transition ease-in-out focus:shadow-cyan-800"}/>
+                    <TbLockPassword className={"absolute text-2xl left-[10px] top-[50%] -translate-y-[50%]"} />
+                </div>
                 { formik.errors.password && formik.touched.password ? <span className={"text-red-500 text-sm"}>{formik.errors.password}</span> : null}
             </div>
-            <button type={"submit"} className={"px-5 py-3 bg-green-400 rounded text-white font-bold"}>Submit</button>
+            <button type={"submit"} className={"px-5 py-3 bg-black w-full rounded text-white font-bold"}>Submit</button>
         </form>
     </>
 });
