9372ec3ccefb274f0163f61ff227d286be59bf87
mounaimMoussaoui <moussaouiabdennaim@gmail.com>
2025-10-19 18:18:33 +0100

    Fix Profile Bugs

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,6 +8,6 @@ export const Profile = React.memo(() => {
 
     return <>
         <h1 className={"py-3 w-fit mx-auto font-bold text-gray-400 text-5xl"}>Profile</h1>
-        <div className={"w-fit mx-auto my-[100px] text-sm"}>Welcome {authStateManagement.user.username}</div>
+        <div className={"w-fit mx-auto my-[100px] text-sm"}>Welcome {authStateManagement?.user?.username}</div>
     </>
 });
