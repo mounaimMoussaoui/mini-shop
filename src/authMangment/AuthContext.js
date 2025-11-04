7e8ce5b33aa2aee49b59254d459a40402991b351
mounaimMoussaoui <moussaouiabdennaim@gmail.com>
2025-11-04 21:18:45 +0100

    delete comment

--- src/authMangment/AuthContext.js
+++ src/authMangment/AuthContext.js
@@ -1,24 +1,3 @@
-// import { create } from "zustand";
-// import { auth } from "../firebase.js";
-// import { signOut } from "firebase/auth";
-//
-//
-//
-//
-//
-// export const useAuthContext = create((Set) => ({
-//     authStateManagement: {},
-//
-//    addLogin: (user) => { Set((state) => ({authStateManagement: {...state.authStateManagement, user} }) )},
-//     logout: () => Set(() => ({
-//         authStateManagement: {},
-//
-//     })),
-// }));
-
-
-
-
 import { create } from "zustand";
 import { auth } from "../firebase.js";
 import { signOut } from "firebase/auth";
