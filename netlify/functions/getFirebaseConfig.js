21fafc8b9cad17ebf96586cd277d85a6f32db6c5
mounaimMoussaoui <moussaouiabdennaim@gmail.com>
2025-11-09 22:25:31 +0100

    fixing the problem of version local dev | production dev by adding netlify.toml file to skip the scanning of keys

--- netlify/functions/getFirebaseConfig.js
+++ netlify/functions/getFirebaseConfig.js
@@ -1,34 +1,7 @@
-// netlify/functions/getFirebaseConfig.js
-exports.handler = async () => {
-    const config = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-    };
+// netlify/functions/getSecret.js
+export async function handler() {
     return {
         statusCode: 200,
-        body: JSON.stringify(config),
+        body: JSON.stringify({ key: process.env.REAL_SECRET_KEY }),
     };
-};
-
-
-
-// export async function handler() {
-//     const config = {
-//         apiKey:  process.env.FIREBASE_API_KEY,
-//         authDomain:  process.env.FIREBASE_AUTH_DOMAIN,
-//         projectId:  process.env.FIREBASE_PROJECT_ID,
-//         storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
-//         messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID,
-//         appId:  process.env.FIREBASE_APP_ID,
-//         measurementId:  process.env.FIREBASE_MEASUREMENT_ID,
-//     };
-//     return {
-//         statusCode: 200,
-//         body: JSON.stringify(config)
-//     };
-// }
-
+}
--- src/firebase.js
+++ src/firebase.js
@@ -1,30 +1,15 @@
 import {getAuth} from "firebase/auth";
-
 import { initializeApp } from "firebase/app";
 
-// const firebaseConfig = {
-//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-// };
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
-// import { initializeApp } from "firebase/app";
-
-export async function initFirebase() {
-    const response = await fetch("../netlify/functions/getFirebaseConfig");
-    const config = await response.json();
-    return initializeApp(config);
-}
-
-
- initFirebase().then((firebase) => {
-     console.log(firebase);
- });
-
-const app = initializeApp();
-
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
