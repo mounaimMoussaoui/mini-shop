5411cc345f61ac18083f2c396ef25fd5d55c0585
mounaimMoussaoui <moussaouiabdennaim@gmail.com>
2025-12-07 21:57:36 +0100

    animation to navbar to be visible when the page loaded

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import {NavBar} from "../components/NavBar.jsx";
 import {Outlet} from "react-router-dom";
 import {Footer} from "../components/Footer.jsx";
+import { motion } from "framer-motion";
 
 export const MainLayout = React.memo(() => {
     return <>
         <NavBar/>
-            <section className="container-fluid min-h-[90vh] p-5 sm:p-0">
+            <motion.section initial={{opacity: 0}} animate={{ opacity: 1, transition: { type: "spring", delay: 0.3 } }} className="container-fluid min-h-[90vh] p-5 sm:p-0">
                 <Outlet/>
-            </section>
+            </motion.section>
         <Footer/>
     </>
 });
